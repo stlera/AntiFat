03e95208c20f7d6caf1f6d61c292d74bede8c6b8
Lera <stlera2010@gmail.com>
2017-12-21 14:49:33 +0200

    code fixing

--- formula.js
+++ formula.js
@@ -6,3 +6,15 @@ function NormalWeight(height, gender){
 		return (height - 100) * 1.15;
 	}
 }
+
+function WeightControl(currentWeight, beginWeight, wishWeight, height, gender){
+	var normalWeight = NormalWeight(height, gender);
+	
+	var leftWeight = currentWeight - wishWeight;
+	
+	/*if(currentWeight <= normalWeight) return false; //should stop weight lost	
+	else*/ if(leftWeight <= 0) return false;          //should stop weight lost
+	else return true;                               //should continue	
+}
+
+
